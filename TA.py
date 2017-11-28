1499ad9550274bfa8d7c522f895c8273fdd72b8a
mo-0day <34004821+mo-0day@users.noreply.github.com>
2017-11-28 14:40:36 +0330

    Update TA.py

--- TA.py
+++ TA.py
@@ -1,7 +1,9 @@
 import json 
 import requests
+import time
+import urllib
 
-TOKEN = "<your-bot-token>"
+TOKEN = "478729127:AAGzgunBG0RQIQ0eMc8h8_lqfuI92d3nHEQ"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
@@ -17,11 +19,12 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
-
 
 def get_last_chat_id_and_text(updates):
     num_updates = len(updates["result"])
@@ -32,9 +35,33 @@ def get_last_chat_id_and_text(updates):
 
 
 def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
-    
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
